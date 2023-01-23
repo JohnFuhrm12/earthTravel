f9cbbd359865246378ea146f9f96cfdfabb497a7
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-23 20:07:47 -0300

    Added Day + Night toggling, and rotation toggling buttons

--- main.js
+++ main.js
@@ -11,8 +11,11 @@ const textureLoader = new THREE.TextureLoader();
 
 const normalTexture = textureLoader.load('./textures/normalMap.jpg');
 const dayTexture = textureLoader.load('./textures/earthMap8K.jpg');
+const nightTexture = textureLoader.load('./textures/earthNightMap.jpg');
 const cloudTexture = textureLoader.load('./textures/cloudsMap4k.jpg');
-const spaceTexture = textureLoader.load('./textures/space.jpg');
+const spaceTexture = textureLoader.load('./textures/space2.jpg');
+
+const specularMap = textureLoader.load('./textures/specularMap.jpg');
 
 const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
@@ -23,8 +26,9 @@ scene.background = spaceTexture;
 const geometry = new THREE.SphereGeometry(0.8, 64, 64);
 const cloudGeometry = new THREE.SphereGeometry(0.805, 64, 64);
 
-const material = new THREE.MeshStandardMaterial();
+const material = new THREE.MeshPhongMaterial();
 material.normalMap = normalTexture;
+material.specularMap = specularMap;
 material.map = dayTexture;
 material.wireframe = false;
 
@@ -45,16 +49,17 @@ cloudSphere.position.set (0, 0, 0);
 scene.add(cloudSphere);
 
 // Lights
-
 const pointLight = new THREE.PointLight(0xffffff, 0.1);
 pointLight.position.set(5, 3, 6);
 pointLight.intensity = 1.2;
 
 scene.add(pointLight);
 
-const PointLightHelper = new THREE.PointLightHelper(pointLight, 1);
+const pointLight2 = new THREE.PointLight(0x1300a6, 0.1);
+pointLight2.position.set(5, 3, 6);
+pointLight2.intensity = 0.3;
 
-const pointLight2 = new THREE.PointLight(0xff0000, 2);
+scene.add(pointLight2);
 
 // Sizes
 const sizes = {
@@ -83,7 +88,8 @@ scene.add(camera);
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
-    alpha: true
+    alpha: true,
+    antialias: true
 });
 
 renderer.setSize(sizes.width, sizes.height);
@@ -104,6 +110,7 @@ const windowHalfX = window.innerWidth / 2;
 const windowHalfY = window.innerHeight / 2;
 
 const clock = new THREE.Clock();
+let rotating = true;
 
 const tick = () => {
     targetX = mouseX * 0.001;
@@ -112,22 +119,66 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime();
 
     // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime;
+    if (!rotating) {
+        sphere.rotation.y = 0 * elapsedTime;
 
-    sphere.rotation.y += 0.1 * (targetX - sphere.rotation.y);
-    sphere.rotation.x += 0.1 * (targetY - sphere.rotation.x);
-    sphere.rotation.z += 0.1* (targetY - sphere.rotation.x);
+        sphere.rotation.y += 0 * (targetX - sphere.rotation.y);
+        sphere.rotation.x += 0 * (targetY - sphere.rotation.x);
+        sphere.rotation.z += 0* (targetY - sphere.rotation.x);
 
-    cloudSphere.rotation.y = 0.1 * elapsedTime;
+        cloudSphere.rotation.y = 0 * elapsedTime;
 
-    cloudSphere.rotation.y += 0.1 * (targetX - sphere.rotation.y);
-    cloudSphere.rotation.x += 0.1 * (targetY - sphere.rotation.x);
-    cloudSphere.rotation.z += 0.1 * (targetY - sphere.rotation.x);
+        cloudSphere.rotation.y += 0 * (targetX - sphere.rotation.y);
+        cloudSphere.rotation.x += 0 * (targetY - sphere.rotation.x);
+        cloudSphere.rotation.z += 0 * (targetY - sphere.rotation.x);
+    } else {
+        sphere.rotation.y = 0.1 * elapsedTime;
+
+        sphere.rotation.y += 0.1 * (targetX - sphere.rotation.y);
+        sphere.rotation.x += 0.1 * (targetY - sphere.rotation.x);
+        sphere.rotation.z += 0.1* (targetY - sphere.rotation.x);
+
+        cloudSphere.rotation.y = 0.1 * elapsedTime;
+
+        cloudSphere.rotation.y += 0.1 * (targetX - sphere.rotation.y);
+        cloudSphere.rotation.x += 0.1 * (targetY - sphere.rotation.x);
+        cloudSphere.rotation.z += 0.1 * (targetY - sphere.rotation.x);
+    };
 
     controls.update();
 
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
-}
+};
 
 tick();
+
+const nightButton = document.querySelector('#nightBtn');
+nightButton.addEventListener("click", changeToNight);
+
+function changeToNight() {
+    material.map = nightTexture;
+    pointLight.intensity = 2.5;
+    material.specularMap = null;
+};
+
+const dayButton = document.querySelector('#dayBtn');
+dayButton.addEventListener("click", changeToDay);
+
+function changeToDay() {
+    material.map = dayTexture;
+    pointLight.intensity = 1.2;
+};
+
+const rotationButton = document.querySelector('#rotationBtn');
+rotationButton.addEventListener("click", toggleRotation);
+
+function toggleRotation() {
+    if (rotating) {
+        rotating = false;
+        rotationButton.innerHTML = 'Start Rotation';
+    } else {
+        rotating = true;
+        rotationButton.innerHTML = 'Stop Rotation';
+    };
+};
